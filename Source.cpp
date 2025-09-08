337a430c4fe8d559d7ad7b74c6505216a531a292
Mostafa Eldaly <eldalymostafa068@gmail.com>
2025-09-08 20:17:52 +0300

    problem-c

--- Source.cpp
+++ Source.cpp
@@ -2,13 +2,23 @@
 using namespace std;
 int main()
 {
-
+	int max = -999999;
 	int num;
-	cout << "please enter the num";
+	int num1;
+	cout << "please enter the number";
 	cin >> num;
-	for (int i = 1; i <= num; i++)
+	cout << "please enter the numbers";
+	for (int i= 0; i < num; i++)
 	{
-		cout <<  i << endl;
+		cin >> num1;
+		if (num1 > max)
+		{
+			max = num1;
+		}
 	}
+	
+	
+	cout << "MAX:" << max;
+
 	return 0;
 }
