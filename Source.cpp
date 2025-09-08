bc3cab5d0fd901472122f49a00dd1df3e64f1ae4
Mostafa Eldaly <eldalymostafa068@gmail.com>
2025-09-08 20:17:10 +0300

    problem-a

--- Source.cpp
+++ Source.cpp
@@ -1,46 +1,14 @@
-#include <iostream>
-
+# include<iostream>
 using namespace std;
+int main()
+{
 
-int main() {
-    int a, b, c;
-    cin >> a >> b >> c;
-
-
-    int min_val = a;
-    if (b < min_val) {
-        min_val = b;
-    }
-    if (c < min_val) {
-        min_val = c;
-    }
-
-    int max_val = a;
-    if (b > max_val) {
-        max_val = b;
-    }
-    if (c > max_val) {
-        max_val = c;
-    }
-
-    int mid_val = (a + b + c) - min_val - max_val;
-
-
-    cout << min_val << endl;
-    cout << mid_val << endl;
-    cout << max_val << endl;
-
-    cout << endl;
-
-    cout << a << endl;
-    cout << b << endl;
-    cout << c << endl;
-
-    return 0;
+	int num;
+	cout << "please enter the num";
+	cin >> num;
+	for (int i = 1; i <= num; i++)
+	{
+		cout <<  i << endl;
+	}
+	return 0;
 }
-
-
-
-
-
-
