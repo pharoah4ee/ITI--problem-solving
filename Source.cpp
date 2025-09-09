a0b44e57c8340479d6bd70e76c187ea60950e241
Mostafa Eldaly <eldalymostafa068@gmail.com>
2025-09-09 16:08:44 +0300

    problem-b

--- Source.cpp
+++ Source.cpp
@@ -1,35 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int getDigitSum(int n) 
-{
-    int sum = 0;
-    while (n > 0) 
-    {
-        sum += n % 10; 
-        n /= 10;       
-    }
-    return sum;
-}
-
 int main() 
 {
-    int n, a, b;
-    cin >> n >> a >> b;
-    int total_sum = 0;
+    int n;
+    cin >> n;
 
-    for (int i = 0; i < n; i++) 
+    if (n < 2) 
     {
-        int digit_sum = getDigitSum(i);
-
-
-        if (digit_sum >= a && digit_sum <= b) 
-        {
-            total_sum += i; 
-        }
+        cout << -1 << endl;
+        return 0;
     }
 
-    cout << total_sum << endl;
+    for (int i = 2; i <= n; i += 2)
+    {
+        cout << i << endl;
+    }
 
     return 0;
 }
