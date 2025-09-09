314bfcf9a23caf001c0dca64818f2fe1ed0e8270
Mostafa Eldaly <eldalymostafa068@gmail.com>
2025-09-09 16:05:58 +0300

    problem-u

--- Source.cpp
+++ Source.cpp
@@ -1,24 +1,35 @@
-# include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-	int max = -999999;
-	int num;
-	int num1;
-	cout << "please enter the number";
-	cin >> num;
-	cout << "please enter the numbers";
-	for (int i= 0; i < num; i++)
-	{
-		cin >> num1;
-		if (num1 > max)
-		{
-			max = num1;
-		}
-	}
-	
-	
-	cout << "MAX:" << max;
 
-	return 0;
+int getDigitSum(int n) 
+{
+    int sum = 0;
+    while (n > 0) 
+    {
+        sum += n % 10; 
+        n /= 10;       
+    }
+    return sum;
+}
+
+int main() 
+{
+    int n, a, b;
+    cin >> n >> a >> b;
+    int total_sum = 0;
+
+    for (int i = 0; i < n; i++) 
+    {
+        int digit_sum = getDigitSum(i);
+
+
+        if (digit_sum >= a && digit_sum <= b) 
+        {
+            total_sum += i; 
+        }
+    }
+
+    cout << total_sum << endl;
+
+    return 0;
 }
